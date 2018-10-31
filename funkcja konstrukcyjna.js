2f05ce3467ee28192ab6d170324233b804e9ce13
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-10-31 14:33:35 +0100

    constructor functions part 2

--- funkcja konstrukcyjna.js
+++ funkcja konstrukcyjna.js
@@ -1,6 +1,16 @@
 function Dog(){
-
+    this.makeSound = makeSound
 }
+
+Dog.prototype.makeSound = function(){
+    console.log(this.sound)
+}
+
+Dog.prototype.sound = 'Woof'
 
 dog0 = Dog()
 const dog1 = new Dog()
+const dog2 = new Dog()
+
+dog1.makeSound()
+
