d8dbadf3e711101a2783ed0d2940ca34572d6511
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-10-31 12:06:47 +0100

    changed makeSound function

--- task2.js
+++ task2.js
@@ -1,7 +1,16 @@
+const makeSound = function(name, lastname){
+    console.log(this.sound + " " + name + " " + lastname)
+}
+
 const cat = {
 name: 'Fluffy',
 sound: 'Miauuu',
-makeSound: function(){
-    console.log(this.sound)
+makeSound: makeSound
 }
-}
+
+cat.makeSound()
+makeSound.apply(cat) //przyjmuje obiekt, który zostanie kontekstem tej funkcji i jakieś argumenty wywowałanego obiektu w []
+makeSound.call(cat) //przyjmuje obiekt, który zostanie kontekstem tej funkcji i jakieś argumenty wywowałanego obiektu bez []
+
+makeSound.apply({sound: Woof})
+makeSound.call({sound: Woof})
