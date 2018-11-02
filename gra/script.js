efd7357062a59f0eec5cafc16f28a42c894025bd
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-02 14:18:48 +0100

    further changes

--- gra/script.js
+++ gra/script.js
@@ -1,13 +1,20 @@
 function Game() {
-    this.boardArray = [
-        [1, 1],
-        [0, 0]
+    this.boardArr = [
+        [1, 1, 1, 1],
+        [0, 0, 1, 0],
+        [1, 0, 1, 1],
+        [1, 0, 1, 0]
     ]
     this.playerPosition = {
         x: 0,
-        y: 1
+        y: 0
     }
 
+    this.init()
+}
+
+Game.prototype.init = function () {
+    this.startListeningToArrows()
     this.render()
 }
 
@@ -15,8 +22,38 @@ Game.prototype.render = function () {
     document.body.innerHTML = ''
 
     this.composeBoard()
+    this.boardArr.forEach((row, i) => {
+        const rowDiv = document.createElement('div')
+        rowDiv.style.height = '20px'
+
+        row.forEach((cell, j) => {
+            this.renderSingleCell(cell, rowDiv)
+        })
+        document.body.appendChild(rowDiv)
+    })
+}
+
+Game.prototype.renderSingleCell = function (cell, rowDiv) {
+    const cellDiv = document.createElement('div')
+
+    cellDiv.style.display = "inline-block"
+    cellDiv.style.width = '20px'
+    cellDiv.style.height = '20px'
+
+    if (cell === 0) cellDiv.style.backgroundColor = 'black'
+    if (cell === 1) cellDiv.style.backgroundColor = 'gray'
+    if (cell === 'P') cellDiv.style.backgroundColor = 'red'
+
+    rowDiv.appendChild(cellDiv)
 }
 
 Game.prototype.composeBoard = function () {
-    this.boardArray[this.playerPosition.y][this.playerPosition.x] = 'P'
+    this.boardArr[this.playerPosition.y][this.playerPosition.x] = 'P'
+}
+
+Game.prototype.startListeningToArrows = function (){
+    window.addEventListener(
+        'keydown',
+        event => console.log(event.key)
+    )
 }
