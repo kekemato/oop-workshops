2ab1b7c3ea1561c17878a9efdc861b1645c95267
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-02 11:04:16 +0100

    added toggle function

--- dzień 2/script.js
+++ dzień 2/script.js
@@ -3,26 +3,40 @@ function SideBarMenu(color, direction) {
     this.isOnTheLeft = direction === 'left' ? true : false
     this.isOnTheRight = direction === 'right' ? true : false
     this.bgColor = color || 'red'
+    this.menuDiv = null
 
     if (!this.isOnTheLeft && !this.isOnTheRight) this.isOnTheLeft = true
+    this.init()
 }
 
-SideBarMenu.prototype.render = function render() {
-    const menuDiv = document.createElement('div')
-    menuDiv.style.width = '200px'
-    menuDiv.style.height = '100vh'
-    menuDiv.style.backgroundColor = this.bgColor
-    menuDiv.style.position = 'fixed'
-    menuDiv.style.top = 0
+SideBarMenu.prototype.render = function () {
+    if (this.menuDiv) this.menuDiv.remove()
 
-    if (this.isOnTheLeft) menuDiv.style.left = 0
-    if (this.isOnTheRight) menuDiv.style.right = 0
-    if (!this.isOpen) menuDiv.style.display = "none"
-    document.body.appendChild(menuDiv)
+    this.menuDiv = document.createElement('div')
+
+    this.menuDiv.style.width = '200px'
+    this.menuDiv.style.height = '100vh'
+    this.menuDiv.style.backgroundColor = this.bgColor
+    this.menuDiv.style.position = 'fixed'
+    this.menuDiv.style.top = 0
+
+    if (this.isOnTheLeft) this.menuDiv.style.left = 0
+    if (this.isOnTheRight) this.menuDiv.style.right = 0
+    if (!this.isOpen) this.menuDiv.style.display = "none"
+    document.body.appendChild(this.menuDiv)
+}
+
+SideBarMenu.prototype.init = function () {
+    this.render()
+}
+
+SideBarMenu.prototype.toggleMenu = function () {
+    this.isOpen ? this.isOpen = false : this.isOpen = true
+    this.render()
 }
 
 const menu1 = new SideBarMenu()
-menu1.render()
 
 const menu2 = new SideBarMenu('green', 'right')
-menu2.render()
+
+menu2.toggleMenu()
