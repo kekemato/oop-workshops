2608f9671b740d136a8cc2193938f456de7c949b
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-02 10:06:29 +0100

    added renders to menu

--- dzień 2/script.js
+++ dzień 2/script.js
@@ -1,11 +1,23 @@
-function SideBarMenu (){
+function SideBarMenu() {
     this.isOpen = true
     this.isOnTheLeft = true
     this.isOnTheRight = false
     this.bgColor = 'red'
 }
 
-const menu1 = new SideBarMenu()
+SideBarMenu.prototype.render = function render() {
+    const menuDiv = document.createElement('div')
+    menuDiv.style.width = '200px'
+    menuDiv.style.height = '100vh'
+    menuDiv.style.backgroundColor = this.bgColor
+    menuDiv.style.position = 'fixed'
+    menuDiv.style.top = 0;
 
-let div = document.createElement('div')
-document.body.appendChild(div)
+    if (this.isOnTheLeft) menuDiv.style.left = 0;
+    if (this.isOnTheRight) menuDiv.style.left = 0;
+    if (!this.isOpen) menuDiv.style.display = "none"
+    document.body.appendChild(menuDiv)
+}
+
+const menu1 = new SideBarMenu()
+menu1.render()
