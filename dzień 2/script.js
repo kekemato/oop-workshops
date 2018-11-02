41ccc66d5ea61cd5f0e58090e825882656ff83db
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-02 10:19:29 +0100

    added second menu

--- dzień 2/script.js
+++ dzień 2/script.js
@@ -1,8 +1,10 @@
-function SideBarMenu() {
+function SideBarMenu(color, direction) {
     this.isOpen = true
-    this.isOnTheLeft = true
-    this.isOnTheRight = false
-    this.bgColor = 'red'
+    this.isOnTheLeft = direction === 'left' ? true : false
+    this.isOnTheRight = direction === 'right' ? true : false
+    this.bgColor = color || 'red'
+
+    if (!this.isOnTheLeft && !this.isOnTheRight) this.isOnTheLeft = true
 }
 
 SideBarMenu.prototype.render = function render() {
@@ -11,13 +13,16 @@ SideBarMenu.prototype.render = function render() {
     menuDiv.style.height = '100vh'
     menuDiv.style.backgroundColor = this.bgColor
     menuDiv.style.position = 'fixed'
-    menuDiv.style.top = 0;
+    menuDiv.style.top = 0
 
-    if (this.isOnTheLeft) menuDiv.style.left = 0;
-    if (this.isOnTheRight) menuDiv.style.left = 0;
+    if (this.isOnTheLeft) menuDiv.style.left = 0
+    if (this.isOnTheRight) menuDiv.style.right = 0
     if (!this.isOpen) menuDiv.style.display = "none"
     document.body.appendChild(menuDiv)
 }
 
 const menu1 = new SideBarMenu()
 menu1.render()
+
+const menu2 = new SideBarMenu('green', 'right')
+menu2.render()
