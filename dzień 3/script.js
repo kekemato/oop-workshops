ba8ed4b6fdd3fb2a5b0e60f204738842a7ea509f
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-05 11:57:28 +0100

    made classes

--- dzień 3/script.js
+++ dzień 3/script.js
@@ -1,43 +1,46 @@
-function ToDo() {
-    this.tasks = []
+class ToDo {
+    constructor (){
+        this.tasks = []
+        this.render()
+    }
 
-    this.render()
+    addTask(text){
+        this.tasks.push(new Task(text))
+
+        this.render()
+    }
+
+    render() {
+        document.body.innerHTML = ''
+        const ul = document.createElement('ul')
+
+        this.makeUI()
+
+        this.tasks.forEach(task => {
+            const li = document.createElement('li')
+            li.innerText = task.text
+            ul.appendChild(li)
+        })
+
+        document.body.appendChild(ul)
+    }
+
+    makeUI() {
+        const input = document.createElement('input')
+        const button = document.createElement('button')
+        button.innerText = 'Dodaj zadanie'
+
+        button.addEventListener('click', () => this.addTask(input.value))
+
+        document.body.appendChild(input)
+        document.body.appendChild(button)
+    }
 }
 
-ToDo.prototype.addTask = function (text) {
-    this.tasks.push(new Task(text))
-
-    this.render()
-}
-
-ToDo.prototype.render = function () {
-    document.body.innerHTML = ''
-    const ul = document.createElement('ul')
-
-    this.makeUI()
-
-    this.tasks.forEach(task => {
-        const li = document.createElement('li')
-        li.innerText = task.text
-        ul.appendChild(li)
-    })
-
-    document.body.appendChild(ul)
-}
-
-ToDo.prototype.makeUI = function() {
-    const input = document.createElement('input')
-    const button = document.createElement('button')
-    button.innerText = 'Dodaj zadanie'
-
-    button.addEventListener('click', () => this.addTask(input.value))
-
-    document.body.appendChild(input)
-    document.body.appendChild(button)
-}
-
-function Task(text) {
-    this.text = text
+class Task{
+    constructor(text){
+        this.text = text
+    }
 }
 
 const ToDo1 = new ToDo()
