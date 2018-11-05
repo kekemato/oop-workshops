38bd1f471fc3a2d2f723cad77bfe7e4257fcf5de
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-05 15:23:30 +0100

    final task solution

--- task/script.js
+++ task/script.js
@@ -1,44 +1,105 @@
-class Animal {
-    constructor(name){
-        this.name = name
+function Calculator() {
+    this.firstNumber = 0
+    this.secondNumber = 0
+}
+
+Calculator.prototype.inputFirst = function (number) {
+    this.firstNumber = number
+}
+
+Calculator.prototype.inputSecond = function (number) {
+    this.secondNumber = number
+}
+
+Calculator.prototype.add = function () {
+    console.log(this.firstNumber + this.secondNumber)
+}
+
+Calculator.prototype.subtract = function () {
+    console.log(this.firstNumber - this.secondNumber)
+}
+
+Calculator.prototype.multiply = function () {
+    if (this.firstNumber === 0 || this.secondNumber === 0) {
+        console.log('One of the numbers is equal to 0, function stopped.')
+    } else {
+        console.log(this.firstNumber * this.secondNumber)
     }
 }
 
-class Cat extends Animal{
-    constructor(name){
-        super(name)
-        this.sound = 'Meowww!'
-    }
-
-    makeSound() {
-        console.log(this.sound)
-    }
-
-    changeSound(newSound){
-        this.sound = newSound
+Calculator.prototype.divide = function () {
+    if (this.firstNumber === 0 || this.secondNumber === 0) {
+        console.log('Zapamiętaj cholero, że nie dzieli się przez zero!')
+    } else {
+        console.log(this.firstNumber / this.secondNumber)
     }
 }
 
-const cat = new Cat('Puszek')
+Calculator.prototype.power = function () {
+    let result = 1
+    for (let i = 0; i < this.secondNumber; i++) {
+        result *= this.firstNumber
+    }
 
-cat.makeSound()
-cat.changeSound('Kłaczek')
-cat.makeSound()
+    console.log(result)
+}
 
-// function Cat(name) {
-//     this.name = name
-//     this.sound = 'Meowww!'
+const calc = new Calculator()
+
+calc.inputFirst(3)
+calc.inputSecond(3)
+
+calc.add()
+calc.subtract()
+calc.multiply()
+calc.divide()
+calc.power()
+
+// class Calculator {
+//     constructor() {
+//         this.firstNumber = 0
+//         this.secondNumber = 0
+//     }
+
+//     inputFirst(number) {
+//         this.firstNumber = number
+//     }
+
+//     inputSecond(number) {
+//         this.secondNumber = number
+//     }
+
+//     add () {
+//         console.log(this.firstNumber + this.secondNumber)
+//     }
+
+//     subtract() {
+//         console.log(this.firstNumber - this.secondNumber)
+//     }
+
+//     multiply() {
+//         if (this.firstNumber === 0 || this.secondNumber === 0) {
+//             console.log('One of the numbers is equal to 0, function stopped.')
+//         } else {
+//             console.log(this.firstNumber * this.secondNumber)
+//         }
+//     }
+
+//     divide() {
+//         if (this.firstNumber === 0 || this.secondNumber === 0) {
+//             console.log('Zapamiętaj cholero, że nie dzieli się przez zero!')
+//         } else {
+//             console.log(this.firstNumber / this.secondNumber)
+//         }
+//     }
 // }
 
-// Cat.prototype.makeSound = function () {
-//     console.log(this.sound)
-// }
-// Cat.prototype.changeSound = function (newSound) {
-//     this.sound = newSound
-// }
+// const calc = new Calculator()
 
-// const cat = new Cat('Puszek')
+// calc.inputFirst(2)
+// calc.inputSecond(3)
 
-// cat.makeSound()
-// cat.changeSound('Kłaczek')
-// cat.makeSound()
+// calc.add()
+// calc.subtract()
+// calc.multiply()
+// calc.divide()
