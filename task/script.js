40f6b5ddacd52c58b0f098ea85027c5aa3014050
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-05 14:10:51 +0100

    third exercise with classes

--- task/script.js
+++ task/script.js
@@ -1,10 +1,44 @@
-const makeSound = () => console.log('Meowww!')
+class Animal {
+    constructor(name){
+        this.name = name
+    }
+}
 
-const makeCat = (name) => ({
-        name: name,
-        makeSound: makeSound
-    })
+class Cat extends Animal{
+    constructor(name){
+        super(name)
+        this.sound = 'Meowww!'
+    }
 
-const cat = makeCat('Puszek')
+    makeSound() {
+        console.log(this.sound)
+    }
+
+    changeSound(newSound){
+        this.sound = newSound
+    }
+}
+
+const cat = new Cat('Puszek')
 
 cat.makeSound()
+cat.changeSound('Kłaczek')
+cat.makeSound()
+
+// function Cat(name) {
+//     this.name = name
+//     this.sound = 'Meowww!'
+// }
+
+// Cat.prototype.makeSound = function () {
+//     console.log(this.sound)
+// }
+// Cat.prototype.changeSound = function (newSound) {
+//     this.sound = newSound
+// }
+
+// const cat = new Cat('Puszek')
+
+// cat.makeSound()
+// cat.changeSound('Kłaczek')
+// cat.makeSound()
