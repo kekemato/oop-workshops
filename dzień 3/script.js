82192c67354c30f695f38f176c9310403db4157c
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-05 10:26:30 +0100

    rendering task

--- dzień 3/script.js
+++ dzień 3/script.js
@@ -1,11 +1,34 @@
 function ToDo() {
     this.tasks = []
+
+    this.render()
 }
 
 ToDo.prototype.addTask = function (text) {
-    new Task(text)
+    this.tasks.push(new Task(text))
+
+    this.render()
 }
 
-function Task(text){
+ToDo.prototype.render = function () {
+    document.body.innerHTML = ''
+    const ul = document.createElement('ul')
+
+
+    this.tasks.forEach(task => {
+        const li = document.createElement('li')
+        li.innerText = task.text
+        ul.appendChild(li)
+    })
+
+    document.body.appendChild(ul)
+}
+
+function Task(text) {
     this.text = text
 }
+
+const ToDo1 = new ToDo()
+
+ToDo1.addTask('wynieś śmieci')
+console.log(ToDo1)
