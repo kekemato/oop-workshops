9dcc6cc4d725dd593c9ab53c75cc1ccdb8f3e7fa
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-06 09:50:18 +0100

    reversed recursion function

--- powtórzenie/script.js
+++ powtórzenie/script.js
@@ -6,3 +6,12 @@ const showNumbers = function innerName(i) {
 }
 
 showNumbers()
+
+const showNumbersReversed = function innerName2(i) {
+    i = i || 0
+    console.log(i)
+
+    if (i > 0 ) innerName2(--i)
+}
+
+showNumbersReversed(10)
