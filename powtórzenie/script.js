81cc2f373a91ca37c8323e29cbef1e0f8fea4707
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-06 11:15:43 +0100

    another task

--- powtórzenie/script.js
+++ powtórzenie/script.js
@@ -1,26 +1,6 @@
-const showNumbers = function innerName(i) {
-    i = i || 0
-    console.log(i)
-
-    if (i < 10 ) innerName(++i)
+function call10Times(func) {
+    for (let i = 0; i < 10; i++)
+    func('BU!')
 }
 
-showNumbers()
-
-const showNumbersReversed = function innerName2(i) {
-    i = (i === undefined) ? 10 : i
-    console.log(i)
-
-    if (i > 0 ) innerName2(--i)
-}
-
-showNumbersReversed(10)
-
-const showNumbersReversed2 = function innerName3(i) {
-    i = i || 0
-    console.log(10 - i)
-
-    if (i < 10 ) innerName3(++i)
-}
-
-showNumbersReversed2()
+call10Times(console.log)
