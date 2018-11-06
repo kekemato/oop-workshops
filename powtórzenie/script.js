b96ba6a09ec1f10bb7d864b92d1efe92c919813d
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-06 09:53:50 +0100

    alternative solution

--- powtórzenie/script.js
+++ powtórzenie/script.js
@@ -15,3 +15,12 @@ const showNumbersReversed = function innerName2(i) {
 }
 
 showNumbersReversed(10)
+
+const showNumbersReversed2 = function innerName3(i) {
+    i = i || 0
+    console.log(10 - i)
+
+    if (i < 10 ) innerName3(++i)
+}
+
+showNumbersReversed2()
