1396ddda7d7d516cf236db14a57c3d06ecfe86b3
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-06 11:59:32 +0100

    my own map function

--- powtórzenie/script.js
+++ powtórzenie/script.js
@@ -1,17 +1,14 @@
-Array.prototype.myOwnForEach = function (func, thisArg) {
-    for(let i = 0; i < this.length; i++){
-    func.call(thisArg, this[i], i, this)
+const numbers = [1, 2, 3, 4, 5]
+
+Array.prototype.myOwnMap = function (func){
+    const newArray = []
+
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(func(this[i], i, this))
     }
+
+    return newArray
 }
 
-let numbers = [1, 2, 3, 4, 5]
-
-numbers.myOwnForEach(
-    function (e) {
-        console.log(e, this)},
-        { whateverProperty: 'this will be this'}
-    )
-
-// const numbers = [1, 2, 3, 4, 5]
-
-// const result = numbers.forEach(element => console.log(element))
+const result = numbers.myOwnMap((e) => 2*e)
+console.log(result)
