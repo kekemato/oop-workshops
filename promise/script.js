01bb05a4143b30a78528233e4a1cc9f758eafec8
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-07 14:05:13 +0100

    fetch task

--- promise/script.js
+++ promise/script.js
@@ -1,7 +1,24 @@
 class UserList {
-    constuctor(container) {
+    constructor(container, number) {
         this.container = container
         this.users = []
+        this.number = number
+
+        this.init()
+    }
+
+    init() {
+        this.render()
+        this.fetchUsers(this.number)
+    }
+
+    fetchUsers(number) {
+        fetch(`https://randomuser.me/api/?results=${number}`)
+            .then(response => response.json())
+            .then(data => {
+                this.users = data.results
+                this.render()
+            })
     }
 
     render() {
@@ -11,9 +28,16 @@ class UserList {
         this.users.forEach(user => {
             const li = document.createElement('li')
 
-            li.innerText = `${name.first} ${name.last}`
+            li.innerText = `${user.name.first} ${user.name.last}`
+
+            ul.appendChild(li)
+            li.addEventListener('click', () => alert(user.email))
         })
 
         this.container.appendChild(ul)
+
     }
+
 }
+
+const list = new UserList(document.body, 6)
