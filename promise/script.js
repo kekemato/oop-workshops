d3b617500595abbbbc86683863b0bd015bb576ac
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-07 13:15:52 +0100

    fetch part 2

--- promise/script.js
+++ promise/script.js
@@ -1,2 +1,8 @@
-fetch('https://randomuser.me/api')
-    .then(response => console.log(response))
+document.body.innerHTML = '<ul>'
+
+fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(data => data.results.forEach((e) => document.body.innerHTML += `<li>${e.name.first}</li>`))
+    .catch(error => console.log('ERROR!', error))
+
+document.body.innerHTML += '</ul>'
